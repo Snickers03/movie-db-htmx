acb483e718705e14284f35b5e3a968bd62fe88f4
Niklas Bialk <niklas.bialk@geberit.com>
2024-03-31 23:47:23 +0200

    fix

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,12 +4,13 @@ const MovieList = () => {
     <div className="w-1/2">
     <div className="flex justify-between mb-3">
       <h1 className="text-2xl">Movie DB</h1>
-        <button className="border-2 border-slate-700 px-3 rounded-md" hx-get="/api/movie" hx-target="#movielist">Refresh</button>
+        <button className="border-2 border-slate-700 px-3 rounded-md" hx-get="/api/movie" hx-target="#movielist" >
+          Filme laden
+        </button>
     </div>
-
-    
-
-    
+    <div id="movielist" className="mt-4">
+      <p className="mt-12 text-center">Drücke auf <span className="underline">Filme laden</span> Button oben rechts um alle Filme anzuzeigen.</p>
+    </div>
   </div>
   )
 }
